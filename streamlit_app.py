a5b677e902eb75bc57fc0eb8053b2134299c9c24
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-20 16:30:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,19 @@
-import pandas as pd
 import streamlit as st
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
+import pandas as pd
 
-def load_data():
-    df = pd.read_csv('CISA_Known_Exploited_Vulnerabilities.csv')
-    return df
+# Load CISA Known Exploited Vulnerabilities catalog as a dataframe
+cisa_catalog = pd.read_csv('CISA_Known_Exploited_Vulnerabilities.csv')
 
-def generate_wordcloud(df):
-    vendors = df['Vendor'].dropna()
-    vendor_counts = vendors.value_counts()
-    wordcloud = WordCloud(background_color='white').generate_from_frequencies(vendor_counts)
-    return wordcloud
+# Get the date the catalog was last updated
+last_updated = cisa_catalog['Date Last Updated'][0]
 
-def main():
-    st.title('CISA Known Exploited Vulnerabilities')
-    st.subheader('Most Common Vendors Wordcloud')
+# Get the 5 most recently added CVEs
+recent_cves = cisa_catalog[['CVE ID', 'Date Added']].sort_values(by=['Date Added'], ascending=False)[:5]
 
-    df = load_data()
-    wordcloud = generate_wordcloud(df)
+# Display the date the catalog was last updated
+st.write("CISA Known Exploited Vulnerabilities catalog was last updated on ", last_updated)
 
-    st.image(wordcloud.to_array())
-
-if __name__ == '__main__':
-    main()
+# Display the 5 most recently added CVEs
+st.write("The 5 most recently added CVEs are:")
+for index, row in recent_cves.iterrows():
+    st.write(row['CVE ID'], " (Added on ", row['Date Added'], ")")
