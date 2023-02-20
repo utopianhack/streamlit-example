4361948680dfa023a3becee1bfc35df87d19e690
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-20 17:00:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,33 @@
 import streamlit as st
-from pyvuln.nvd import NVD
+import requests
+from bs4 import BeautifulSoup
 
-def get_cve_info(cve):
-    nvd = NVD.from_feeds()
-    cve_item = nvd.get_by_cve_id(cve)
-    if cve_item:
-        return cve_item['description'], cve_item['published_date']
-    else:
-        return None, None
+def get_cves(url):
+    # Make a GET request to the URL
+    response = requests.get(url)
 
-    # App title
-st.title('CVE Information App')
+    # Use BeautifulSoup to parse the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# CVE input field
-cve = st.text_input('Enter CVE')
+    # Find all the CVE elements on the page
+    cve_elements = soup.find_all('a', href=lambda href: href and 'cve.mitre.org/cgi-bin/cvename.cgi' in href)
 
-# Button to get CVE info
-if st.button('Get CVE Info'):
-    description, published_date = get_cve_info(cve)
-    if description and published_date:
-        st.write('Description:', description)
-        st.write('Published date:', published_date)
-    else:
-        st.write('CVE not found')
+    # Extract the CVE numbers from the elements
+    cves = [cve.text for cve in cve_elements]
+
+    return cves
+# Define the Streamlit app
+def app():
+    # Add a sidebar to the app
+    st.sidebar.title('Enter a URL')
+    url = st.sidebar.text_input('')
+
+    # Create a button to start the scraping process
+    if st.sidebar.button('Get CVEs'):
+        # Scrape the CVEs
+        cves = get_cves(url)
+
+        # Display the results in the main section
+        st.write('CVEs found:')
+        for cve in cves:
+            st.write(f'- {cve}')
