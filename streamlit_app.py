feffd8d898454f16cd454837503913d6054cda10
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-20 16:20:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,20 @@
 import streamlit as st
 from urllib.parse import urlparse
 
-st.title("Extract Domain from URL")
-
-# Get URL input from user
-url = st.text_input("Enter a URL:")
-
-# Extract domain from URL
-if url:
+def get_domain(url):
     parsed_url = urlparse(url)
-    domain = parsed_url.netloc
-    st.write("Domain:", domain)
+    domain = parsed_url.netloc.split('.')
+    if len(domain) == 2:
+        return domain[0], domain[1]
+    elif len(domain) == 3:
+        return domain[1], domain[2]
+    else:
+        return None, None
+
+st.title("Domain Extractor")
+
+url = st.text_input("Enter a URL")
+if url:
+    top_level_domain, second_level_domain = get_domain(url)
+    st.write(f"Top Level Domain: {top_level_domain}")
+    st.write(f"Second Level Domain: {second_level_domain}")
