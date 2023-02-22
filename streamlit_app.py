1f8d1d7eceb2424d91923d3dd0691aff415b3365
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-22 12:16:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,20 +6,20 @@ st.title("Extract Domain Names and TLDs from CSV")
 
 # Create file uploader
 uploaded_file = st.file_uploader("Choose a file", type=["csv"])
-if uploaded_file is not None:
-    # Read CSV file
-    df = pd.read_csv(uploaded_file)
+
+# Read CSV file
+df = pd.read_csv(uploaded_file)
     
-    # Extract domain names and TLDs
-    domains = []
-    tlds = []
+# Extract domain names and TLDs
+domains = []
+tlds = []
 
-    for index, row in df.iterrows():
-        domain = extract(row['Website']).domain
-        tld = extract(row['Website']).suffix
-        domains.append(domain)
-        tlds.append(tld)
+for index, row in df.iterrows():
+    domain = extract(row['Website']).domain
+    tld = extract(row['Website']).suffix
+    domains.append(domain)
+    tlds.append(tld)
 
-    # Create new dataframe and display in Streamlit app
-    new_df = pd.DataFrame({'Domain': domains, 'TLD': tlds})
-    st.write(new_df)
+# Create new dataframe and display in Streamlit app
+new_df = pd.DataFrame({'Domain': domains, 'TLD': tlds})
+st.write(new_df)
