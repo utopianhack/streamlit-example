f5cb29bf560e6a7ea1f0efbd5b4d4a94cf9356cd
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-22 11:38:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,32 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import tldextract
+import matplotlib.pyplot as plt
 
-def extract_tld(url):
-    extracted = tldextract.extract(url)
-    return extracted.suffix
+def extract_urls(df):
+    urls = []
+    for index, row in df.iterrows():
+        urls_in_row = tldextract.extract(row[0])
+        urls.append(urls_in_row.domain + '.' + urls_in_row.suffix)
+    return urls
+
+def count_tlds(urls):
+    tlds = pd.Series(urls).value_counts()
+    return tlds
+
+def display_histogram(tlds):
+    plt.bar(tlds.index, tlds.values)
+    plt.xticks(rotation=90)
+    st.pyplot()
 
 def main():
-    st.title("Most Frequent TLDs")
-    st.write("Enter a list of URLs to see the most frequent TLDs.")
-
-    # Get the list of URLs from the user
-    input_urls = st.text_area("Enter URLs, one per line")
-
-    # Convert the input URLs to a list
-    urls = input_urls.split("\n")
-
-    # Extract the TLDs from the URLs
-    tlds = [extract_tld(url) for url in urls]
-
-    # Count the frequencies of each TLD
-    tld_counts = pd.Series(tlds).value_counts()
-
-    # Display the histogram of the most frequent TLDs
-    st.bar_chart(tld_counts.head(10))
+    st.title("URL TLD Histogram")
+    file = st.file_uploader("Upload file", type="csv")
+    if file is not None:
+        df = pd.read_csv(file)
+        urls = extract_urls(df)
+        tlds = count_tlds(urls)
+        display_histogram(tlds)
 
 if __name__ == "__main__":
     main()
