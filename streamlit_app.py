43092ed63e7e73ef1e2f743c80d0bd409f54da4c
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-22 16:53:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,14 @@
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
-from wordcloud import WordCloud
+import feedparser
 
-def scrape_text(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    text = ' '.join([p.text for p in soup.find_all('p')])
-    return text
+st.title("Get RSS URL from a Website")
 
-def generate_wordcloud(text):
-    wordcloud = WordCloud(width=800, height=800, background_color='white', min_font_size=10).generate(text)
-    return wordcloud.to_image()
+url = st.text_input("Enter a website URL")
 
-# Set the title of the app
-st.title('Web Scraper and Wordcloud Generator')
-
-# Add a text input for the URL
-url = st.text_input('Enter the URL of the webpage you want to scrape:')
-
-# Add a button to start scraping
-if st.button('Scrape'):
-    # Call the scrape_text function to get the text
-    text = scrape_text(url)
-
-    # Generate the wordcloud and display it
-    image = generate_wordcloud(text)
-    st.image(image, use_column_width=True)
+if url:
+    feed = feedparser.parse(url)
+    if 'rss' in feed.version.lower():
+        st.write("The RSS URL for the provided website is: ")
+        st.write(feed.feed.link)
+    else:
+        st.write("No RSS feed found for the provided website.")
