c3ef638daa3ca1d658d00ea90539e7d94c9c6566
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-22 10:30:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,11 @@
 import streamlit as st
-import pandas as pd
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
+import tldextract
 
-# Load the data from the CSV file
-data = pd.read_csv('path/to/known_exploits.csv')
+st.title("TLD Extractor")
 
-# Group the data by product and count the number of vulnerabilities
-product_counts = data.groupby('Product').count()['ID'].sort_values(ascending=False)
+url = st.text_input("Enter a URL:")
 
-# Generate the wordcloud
-wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(product_counts)
-
-# Define the Streamlit app
-st.title("CISA's Known Exploited Vulnerabilities - Most Frequent Products")
-st.set_option('deprecation.showPyplotGlobalUse', False)
-st.pyplot(plt.figure(figsize=(20,10)))
+if url:
+    with st.spinner('Extracting TLD...'):
+        extracted = tldextract.extract(url)
+        st.success(f"The TLD of {url} is {extracted.suffix}")
