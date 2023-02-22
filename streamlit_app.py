06e762e29213f9e2836bd653f5b1ca38e6ec885a
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-22 10:36:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import tldextract
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.title("TLD Extractor")
 
-url = st.text_input("Enter a URL:")
+st.title("Extract URLs and plot their TLDs")
 
-if url:
-    with st.spinner('Extracting TLD...'):
-        extracted = tldextract.extract(url)
-        st.success(f"The TLD of {url} is {extracted.suffix}")
+# Take text input
+text = st.text_area("Enter text", height=200)
+
+# Extract URLs and plot TLDs
+if st.button("Extract URLs"):
+    urls = []
+    for line in text.split('\n'):
+        urls += re.findall("(?P<url>https?://[^\s]+)", line)
+    tlds = [tldextract.extract(url).suffix for url in urls]
+    tld_counts = pd.Series(tlds).value_counts()
+    st.bar_chart(tld_counts)
