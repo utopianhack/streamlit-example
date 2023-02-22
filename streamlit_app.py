a87e49dbf8b63c69af9faece4f4ec587b2d22cad
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-22 10:50:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,13 @@
 import streamlit as st
-import re
 
-def extract_domain(text):
-    pattern = r"(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    domain_list = re.findall(pattern, text)
-    return [domain[0] for domain in domain_list]
+def word_char_count(text):
+    words = len(text.split())
+    chars = len(text)
+    return words, chars
 
-def main():
-    st.title("Extract Domain Names from Text")
-    input_text = st.text_input("Enter Text:")
-    if input_text:
-        domain_list = extract_domain(input_text)
-        if domain_list:
-            st.write("Domain Names:")
-            for domain in domain_list:
-                st.write(domain)
-        else:
-            st.write("No domain names found in the text")
-
-if __name__ == '__main__':
-    main()
+st.title("Word and Character Count App")
+user_input = st.text_area("Enter text here:")
+if st.button("Count!"):
+    words, chars = word_char_count(user_input)
+    st.write(f"Word Count: {words}")
+    st.write(f"Character Count: {chars}")
