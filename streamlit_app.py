1316611cddc58da03a2351cbb378ecb82c16305b
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-23 08:23:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,16 @@
 import streamlit as st
-import pandas as pd
 import tldextract
 
-def get_domains(df):
-    domains = set()
-    for url in df['url']:
-        ext = tldextract.extract(url)
-        domain = ext.domain + '.' + ext.suffix
-        domains.add(domain)
-    return sorted(list(domains))
+st.title('URL Domain and TLD Extractor')
 
-def main():
-    st.title("Domain Name Extractor")
-    st.write("Upload a .csv file to extract the domain names it contains.")
+url = st.text_input('Enter a URL:')
 
-    # Upload file
-    uploaded_file = st.file_uploader("Choose a file")
+if url:
+    extracted_url = tldextract.extract(url)
+    domain = extracted_url.domain
+    tld = extracted_url.suffix
+    st.write(f'Domain: {domain}')
+    st.write(f'TLD: {tld}')
 
-    if uploaded_file is not None:
-        # Read file
-        df = pd.read_csv(uploaded_file)
-
-        # Extract domain names
-        domains = get_domains(df)
-
-        # Display domain names
-        st.write("Domain names:")
-        for domain in domains:
-            st.write(domain)
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    st.run_app()
