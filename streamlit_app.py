2eaf3bb45b3cada6456443b177da3a1538ed5f55
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-25 17:32:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,30 @@
 import streamlit as st
-import pandas as pd
-import tldextract
+import nltk
+from nltk import ngrams
+from collections import Counter
+import matplotlib.pyplot as plt
 
-def extract_tld(url):
-    extracted = tldextract.extract(url)
-    return extracted.suffix
+def generate_ngrams(text, n):
+    words = nltk.word_tokenize(text)
+    n_grams = ngrams(words, n)
+    return [ ' '.join(grams) for grams in n_grams]
 
-def main():
-    st.title("Jonathan's Greatest App Stolen From ChatGPT")
-    st.write("Enter a list of URLs to see the most frequent TLDs.")
+st.title("N-gram Histogram")
 
-    # Get the list of URLs from the user
-    input_urls = st.text_area("Enter URLs, one per line")
+text = st.text_area("Enter some text to analyze:", height=200)
 
-    # Convert the input URLs to a list
-    urls = input_urls.split("\n")
+n = st.slider("Select the length of the n-grams:", min_value=1, max_value=5, value=2)
 
-    # Extract the TLDs from the URLs
-    tlds = [extract_tld(url) for url in urls]
-
-    # Count the frequencies of each TLD
-    tld_counts = pd.Series(tlds).value_counts()
-
-    # Display the histogram of the most frequent TLDs
-    st.bar_chart(tld_counts.head(10))
-
-if __name__ == "__main__":
-    main()
+if st.button("Generate Histogram"):
+    ngram_list = generate_ngrams(text.lower(), n)
+    ngram_counts = Counter(ngram_list)
+    sorted_ngrams = sorted(ngram_counts.items(), key=lambda x: x[1], reverse=True)
+    
+    if ngram_list:
+        st.write(f"Most common {n}-grams:")
+        fig, ax = plt.subplots()
+        ax.bar([x[0] for x in sorted_ngrams[:10]], [x[1] for x in sorted_ngrams[:10]])
+        plt.xticks(rotation=90)
+        st.pyplot(fig)
+    else:
+        st.write("Please enter some text to analyze.")
