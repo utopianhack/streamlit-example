22b6f100c9c3e6125ad7f0f883e02ce44f33f8a4
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-26 16:43:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,27 +6,23 @@ import plotly.express as px
 df = pd.read_csv("https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv")
 
 # Add a sidebar
-st.sidebar.title("Select options")
+st.sidebar.title("Select Plot Type")
 
 # Add a dropdown menu to select the column
-column = st.sidebar.selectbox("Select a column", df.columns)
+column = st.sidebar.selectbox("Select Column", df.columns)
 
 # Add a dropdown menu to select the plot type
-plot_type = st.sidebar.selectbox(
-    "Select a plot type",
-    {
-        "Bar Chart": px.bar,
-        "Pie Chart": px.pie,
-        "DataFrame": st.write,
-    },
-)
+plot_type = st.sidebar.selectbox("Select Plot Type", ["Bar Chart", "Pie Chart"])
 
 # Count the frequency of the selected column
 counts = df[column].value_counts()
 
 # Create a chart based on the user's selection
-if plot_type == "DataFrame":
-    plot_type(counts)
+if plot_type == "Bar Chart":
+    fig = px.bar(x=counts.index, y=counts.values)
 else:
-    fig = plot_type(x=counts.index, y=counts.values)
-    st.plotly_chart(fig)
+    fig = px.pie(names=counts.index, values=counts.values)
+
+# Display the chart using Streamlit
+st.plotly_chart(fig)
+st.dataframe(df.[str(column)])
