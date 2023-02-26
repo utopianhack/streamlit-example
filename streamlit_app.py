56ad539fdc885538487036d3964ee55d5578994d
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-26 16:15:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,10 @@ import plotly.express as px
 df = pd.read_csv("https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv")
 
 # Group the data by product and count the occurrences
-product_counts = df.groupby("product").size().reset_index(name="Counts")
+product_counts = df.groupby("product").size().reset_index(name="counts")
 
 # Sort the products by counts in descending order
-product_counts = product_counts.sort_values("Counts", ascending=False)
+product_counts = product_counts.sort_values("counts", ascending=False)
 
 # Create a histogram using Plotly Express
 fig = px.histogram(product_counts.head(10), x="product", y="counts", title="Most Frequent Products")
