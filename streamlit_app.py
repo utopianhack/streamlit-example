195b2f57560d0470fad0b45a2a453303a557ffc4
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-26 16:14:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import plotly.express as px
 df = pd.read_csv("https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv")
 
 # Group the data by product and count the occurrences
-product_counts = df.groupby("Product").size().reset_index(name="Counts")
+product_counts = df.groupby("product").size().reset_index(name="Counts")
 
 # Sort the products by counts in descending order
 product_counts = product_counts.sort_values("Counts", ascending=False)
