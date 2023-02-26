48cada0c716eee7ee9f589a0fe8873de93bbfcab
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-26 16:12:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,18 @@
 import streamlit as st
-import nltk
-from nltk import ngrams
-from collections import Counter
-import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.express as px
 
-def generate_ngrams(text, n):
-    words = nltk.word_tokenize(text)
-    n_grams = ngrams(words, n)
-    return [ ' '.join(grams) for grams in n_grams]
+# Load the CSV file
+df = pd.read_csv("https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv")
 
-st.title("N-gram Histogram")
+# Group the data by product and count the occurrences
+product_counts = df.groupby("Product").size().reset_index(name="Counts")
 
-text = st.text_area("Enter some text to analyze:", height=200)
+# Sort the products by counts in descending order
+product_counts = product_counts.sort_values("Counts", ascending=False)
 
-n = st.slider("Select the length of the n-grams:", min_value=1, max_value=5, value=2)
+# Create a histogram using Plotly Express
+fig = px.histogram(product_counts.head(10), x="Product", y="Counts", title="Most Frequent Products")
 
-if st.button("Generate Histogram"):
-    ngram_list = generate_ngrams(text.lower(), n)
-    ngram_counts = Counter(ngram_list)
-    sorted_ngrams = sorted(ngram_counts.items(), key=lambda x: x[1], reverse=True)
-    
-    if ngram_list:
-        st.write(f"Most common {n}-grams:")
-        fig, ax = plt.subplots()
-        ax.bar([x[0] for x in sorted_ngrams[:10]], [x[1] for x in sorted_ngrams[:10]])
-        plt.xticks(rotation=90)
-        st.pyplot(fig)
-    else:
-        st.write("Please enter some text to analyze.")
+# Display the histogram using Streamlit
+st.plotly_chart(fig)
