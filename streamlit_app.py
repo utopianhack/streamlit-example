1ba7d95a5a12d1f8bd0c855629ccc1fd7466f756
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-26 22:30:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,15 @@
 import streamlit as st
-from nvdlib.nvd import NVD
-from pycvesearch import CVESearch
+import re
 
-st.title("CVE Information App")
+def highlight_cves(text):
+    cve_pattern = r'CVE-\d{4}-\d{4,7}'  # regular expression pattern for CVEs
+    highlighted_text = re.sub(cve_pattern, r'<mark>\g<0></mark>', text)
+    return highlighted_text
 
-cve_id = st.text_input("Enter the CVE ID")
+st.title('CVE Highlighter')
 
-if cve_id:
-    nvd = NVD()
-    cve = nvd.cve(cve_id)
-    if cve:
-        st.write("### Description")
-        st.write(cve.description)
-        st.write("### CVSS Score")
-        st.write(cve.cvss_score)
-    else:
-        st.write("CVE not found in NVD")
+text = st.text_area('Enter text', '')
 
-    cve_search = CVESearch()
-    cve_info = cve_search.id(cve_id)
-    if cve_info:
-        st.write("### References")
-        st.write(cve_info.get("references", ""))
-    else:
-        st.write("CVE not found in CVE Search")
+if st.button('Highlight CVEs'):
+    highlighted_text = highlight_cves(text)
+    st.markdown(highlighted_text, unsafe_allow_html=True)
