758a823041597ff88af30d9f740cbbf770da7950
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-02-28 18:33:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,13 @@ import streamlit as st
 def highlight_iocs(text):
     email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     ip_regex = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+    domain_regex = r'\b(?:https?://)?(?:www\.)?([A-Za-z0-9.-]+)\.[A-Z|a-z]{2,}\b'
     cve_regex = r'\bCVE-\d{4}-\d{4,7}\b'
     asn_regex = r'\bAS\d+\b'
     
     email_matches = re.findall(email_regex, text)
     ip_matches = re.findall(ip_regex, text)
+    domain_matches = re.findall(domain_regex, text)
     cve_matches = re.findall(cve_regex, text)
     asn_matches = re.findall(asn_regex, text)
     
@@ -18,6 +20,9 @@ def highlight_iocs(text):
         
     for match in ip_matches:
         text = text.replace(match, f'<span style="background-color: #ff00ff">{match}</span>')
+
+    for match in domain_matches:
+        text = text.replace(match, f'<span style="background-color: #00ffff">{match}</span>')
 
     for match in cve_matches:
         text = text.replace(match, f'<span style="background-color: #00ff00">{match}</span>')
@@ -37,19 +42,14 @@ def main():
         
         email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
         ip_regex = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+        domain_regex = r'\b(?:https?://)?(?:www\.)?([A-Za-z0-9.-]+)\.[A-Z|a-z]{2,}\b'
         cve_regex = r'\bCVE-\d{4}-\d{4,7}\b'
         asn_regex = r'\bAS\d+\b'
         
         email_matches = [(match, 'Email') for match in re.findall(email_regex, input_text)]
         ip_matches = [(match, 'IP Address') for match in re.findall(ip_regex, input_text)]
+        domain_matches = [(match, 'Domain Name') for match in re.findall(domain_regex, input_text)]
         cve_matches = [(match, 'CVE') for match in re.findall(cve_regex, input_text)]
         asn_matches = [(match, 'ASN') for match in re.findall(asn_regex, input_text)]
         
-        data = {'Type': [match[1] for match in email_matches + ip_matches + cve_matches + asn_matches],
-                'Value': [match[0] for match in email_matches + ip_matches + cve_matches + asn_matches]}
-        
-        df = pd.DataFrame(data)
-        st.write(df)
-
-if __name__ == '__main__':
-    main()
+        data = {'Type': [match[1] for match in email_matches + ip_matches +
