2f8475f7b1b34b4ae20e9cc5386d900ad53e273c
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-05-28 09:09:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,6 @@ col1, col2, col3, col4 = st.columns(4)
 with col1:
     st.metric("Last Update", most_recent, f"{num_added_on_most_recent} CVEs Added")
     st.write(most_recent_cves)
-    cve_cleaned = re.sub(r"[\[\]']", "", df["cveID"])
     st.write(f"https://nvd.nist.gov/vuln/detail/{most_recent_cves_cleaned}")
     #st.table(most_recent_cves_details)
 
