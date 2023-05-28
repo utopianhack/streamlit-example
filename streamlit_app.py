197fd5797e9f9044b33b8b7ecb3e3b372a6b5599
utopianhack <125885803+utopianhack@users.noreply.github.com>
2023-05-28 08:00:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,8 @@ col1, col2, col3, col4 = st.columns(4)
 # Display the metrics in separate columns
 with col1:
     st.metric("Last Update", most_recent, f"{num_added_on_most_recent} CVEs Added")
-    st.write(most_recent_cves)
+    #st.write(most_recent_cves)
+    st.dataframe(most_recent)
 
 with col2:
     st.metric("Product With Most CISA KEV CVEs", most_frequent_product, f"{frequency} CVEs")
